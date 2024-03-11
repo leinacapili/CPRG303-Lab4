cfd4c5ec44ad20a738f4d782f0c2a482d961f137
lei.caps <123599570+leinacapili@users.noreply.github.com>
2024-03-10 18:50:04 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
+  const [tasks, setTask] = useState([ 'Do laundry', 'Go to gym', 'Walk dog']);
+
+  const addTask = (taskText) => {
+    setTask([...tasks, taskText]);
+  };
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm addTask={addTask}/>
+    </SafeAreaView>
+  );
+}
+
+export default App;
